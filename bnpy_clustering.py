d3049de1119bcb6e9b0669fd8c9eeea13b8a630d
Yue Zhang <yjzhang@cs.washington.edu>
2018-10-05 17:02:01 -0700

    timing

--- bnpy_clustering.py
+++ bnpy_clustering.py
@@ -42,6 +42,7 @@ def bnpy_select_clusters(data, max_cells=50000):
     return selected_k, cluster_labels
 
 if __name__ == '__main__':
+    import time
     # load/subset data
     data_mat = scipy.io.loadmat('../data/10x_pooled_400.mat')
     data = data_mat['data']
@@ -50,16 +51,20 @@ if __name__ == '__main__':
 
     # run bnpy clustering?
     true_labels = data_mat['labels'].flatten()
+    t0 = time.time()
     selected_k, labels = bnpy_select_clusters(data_subset)
     print(selected_k)
     print('nmi: ' + str(nmi(true_labels, labels)))
+    print('time: ' + str(time.time() - t0))
 
     data_mat_2 = scipy.io.loadmat('../../uncurl_python/data/SCDE_k2_sup.mat')
     data = data_mat_2['Dat']
+    t0 = time.time()
     selected_k, labels = bnpy_select_clusters(data)
     true_labels = data_mat_2['Lab'].flatten()
     print(selected_k)
     print('nmi: ' + str(nmi(true_labels, labels)))
+    print('time: ' + str(time.time() - t0))
 
     # Zeisel 7-cluster dataset
     data_mat_3 = scipy.io.loadmat('../../uncurl_python/data/GSE60361_dat.mat')
@@ -67,9 +72,11 @@ if __name__ == '__main__':
     gene_subset = uncurl.max_variance_genes(data)
     data_subset = data[gene_subset, :]
     true_labels = data_mat_3['ActLabs'].flatten()
+    t0 = time.time()
     selected_k, labels = bnpy_select_clusters(data)
     print(selected_k)
     print('nmi: ' + str(nmi(true_labels, labels)))
+    print('time: ' + str(time.time() - t0))
 
     # spinal cord data
     data_4 = scipy.io.mmread('../../split_seq_analysis/spinal_cord.mtx.gz')
@@ -81,10 +88,12 @@ if __name__ == '__main__':
     true_labels = pd.read_table('../../split_seq_analysis/spinal_cluster_assignment.txt', header=None)
     true_labels = true_labels.as_matrix().astype(str)
     true_labels = true_labels.flatten()
+    t0 = time.time()
     selected_k, labels = bnpy_select_clusters(data_subset)
     # there are 45 labels in the "true" dataset.
     print(selected_k)
     print('nmi: ' + str(nmi(true_labels, labels)))
+    print('time: ' + str(time.time() - t0))
 
     # 8k single-cell
     data_5 = scipy.io.mmread('../../uncurl_test_datasets/10x_pure_pooled/data_8000_cells.mtx.gz')
@@ -92,6 +101,8 @@ if __name__ == '__main__':
     gene_subset = uncurl.max_variance_genes(data_5)
     data_subset = data_5[gene_subset, :]
     true_labels = np.loadtxt('../../uncurl_test_datasets/10x_pure_pooled/labels_8000_cells.txt').astype(int).flatten()
+    t0 = time.time()
     selected_k, labels=bnpy_select_clusters(data_subset)
     print(selected_k)
     print('nmi: ' + str(nmi(true_labels, labels)))
+    print('time: ' + str(time.time() - t0))
--- gap_score.py
+++ gap_score.py
@@ -34,7 +34,6 @@ def calculate_gap(data, clustering, km, B=50):
 
     Returns two results: the gap score, and s_k.
     """
-    Nd = data.shape[1]
     k = len(set(clustering))
     Wk = km.inertia_
     mins, maxes = calculate_bounding_box(data)
@@ -69,7 +68,7 @@ def run_gap_k_selection(data, k_min=1, k_max=15, B=50,
         clusters = km.fit_predict(data)
         gap, sk = calculate_gap(data, clusters, km, B=B)
         if len(gap_vals) > 1:
-            if gap_vals[-1] >= gap - skip*sk:
+            if gap_vals[-1] >= gap - (skip+1)*sk:
                 min_i = i
                 min_k = k_range[i-1]
                 break
@@ -92,45 +91,52 @@ def run_gap_k_selection(data, k_min=1, k_max=15, B=50,
                 return min_k, gap_vals, sk_vals
         gap_vals.append(gap)
         sk_vals.append(sk)
-    return min_k, gap_vals, sk_vals
+    return k, gap_vals, sk_vals
 
 
 if __name__ == '__main__':
     import scipy.io
+    import time
 
     data_mat = scipy.io.loadmat('../data/10x_pooled_400.mat')
     data = data_mat['data']
     data_tsvd = preproc_data(data)
     #max_k, bic_vals = run_bic_k_selection(data_tsvd)
+    t0 = time.time()
     max_k, gap_vals, sk_vals = run_gap_k_selection(data_tsvd, 
             k_min=1, k_max=50, skip=5, B=5)
     print(max_k)
     print(gap_vals)
     print(sk_vals)
+    print('time: ' + str(time.time() - t0))
 
     data_mat_2 = scipy.io.loadmat('../../uncurl_python/data/SCDE_k2_sup.mat')
     data = data_mat_2['Dat']
     data_tsvd = preproc_data(data)
     #max_k, bic_vals = run_bic_k_selection(data_tsvd)
     #max_k, gap_vals, sk_vals = run_gap_k_selection(data_tsvd, B=5)
+    t0 = time.time()
     max_k, gap_vals, sk_vals = run_gap_k_selection(data_tsvd, 
             k_min=1, k_max=50, skip=5, B=5)
  
     print(max_k)
     print(gap_vals)
     print(sk_vals)
+    print('time: ' + str(time.time() - t0))
 
     data_mat_3 = scipy.io.loadmat('../../uncurl_python/data/GSE60361_dat.mat')
     data = data_mat_3['Dat']
     data_tsvd = preproc_data(data)
     #max_k, bic_vals = run_bic_k_selection(data_tsvd)
     #max_k, gap_vals, sk_vals = run_gap_k_selection(data_tsvd, B=5)
+    t0 = time.time()
     max_k, gap_vals, sk_vals = run_gap_k_selection(data_tsvd, 
             k_min=1, k_max=50, skip=5, B=5)
  
     print(max_k)
     print(gap_vals)
     print(sk_vals)
+    print('time: ' + str(time.time() - t0))
 
     data_4 = scipy.io.mmread('../../split_seq_analysis/spinal_cord.mtx.gz')
     import scipy.sparse
@@ -138,23 +144,27 @@ if __name__ == '__main__':
     data_tsvd = preproc_data(data_4)
     #max_k, bic_vals = run_bic_k_selection(data_tsvd)
     #max_k, gap_vals, sk_vals = run_gap_k_selection(data_tsvd, B=5)
+    t0 = time.time()
     max_k, gap_vals, sk_vals = run_gap_k_selection(data_tsvd, 
             k_min=1, k_max=50, skip=5, B=5)
  
     print(max_k)
     print(gap_vals)
     print(sk_vals)
+    print('time: ' + str(time.time() - t0))
 
     data_5 = scipy.io.mmread('../../uncurl_test_datasets/10x_pure_pooled/data_8000_cells.mtx.gz')
     data_5 = scipy.sparse.csc_matrix(data_5)
     data_tsvd = preproc_data(data_5)
     #max_k, bic_vals = run_bic_k_selection(data_tsvd)
     #max_k, gap_vals, sk_vals = run_gap_k_selection(data_tsvd, B=5)
+    t0 = time.time()
     max_k, gap_vals, sk_vals = run_gap_k_selection(data_tsvd, 
             k_min=1, k_max=50, skip=5, B=5)
  
     print(max_k)
     print(gap_vals)
     print(sk_vals)
+    print('time: ' + str(time.time() - t0))
 
 
